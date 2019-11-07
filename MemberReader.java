1f5d6a3d8802aa710783b77e2a6a3700a3123edc
Michael Bernasol <mfbernasol@gmail>
2019-11-06 19:25:47 -0600

    Create MemberReader class to read text file

---
+++ MemberReader.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.File;
+
+public class MemberReader {
+    public static ArrayList<Member> readMembersFromFile(String fname){
+        try{
+            ArrayList<Member> members = new ArrayList<Member>();
+            Scanner sc = new Scanner(new File(fname));
+            String line;
+            String[] parts;
+
+            while(sc.hasNextLine()){
+                line = sc.nextLine();
+                parts = line.split("\t");
+
+
+                members.add(new Member(parts[0],parts[1],Integer.parseInt(parts[2]),Integer.parseInt(parts[3]),Integer.parseInt(parts[4]),Integer.parseInt(parts[5]),Integer.parseInt(parts[6]),parts[7],parts[8],parts[9]));
+            }
+            sc.close();
+            return members;
+
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+}
