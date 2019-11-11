0bdfa3b2e37154afddabd22c277d102e5b0c910c
Michael Bernasol <mfbernasol@gmail>
2019-11-11 13:19:57 -0600

    Created writeMembersToTextFile function

--- MemberWriter.java
+++ MemberWriter.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
+import java.io.*;
 public class MemberWriter {
-
     //prints members to screen
+
     public static void printMembersToScreen(ArrayList<Member> members){
         for(Member m: members){
             System.out.println(m);
@@ -8,14 +10,25 @@ public class MemberWriter {
     }
 
 
+    //function to write members to a tab-delimited file
+    public static boolean writeMembersToTextFIle(String fname, ArrayList<Member> members){
+        try{
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fname)));
+            for(Member m: members){
+                pw.println(m);
+            }
+            pw.close();
+            return true;
+        }catch(Exception ex){
+            return false;
+        }
+    }
+
+
     //function to write to binary
 
 
-    
     //function to write to xml
-
-
-
 
     //function to write to JSON
 
