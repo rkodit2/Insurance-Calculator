f227b45c14a4d90fd5410d3146d1f7934d814070
Michael Bernasol <mfbernasol@gmail>
2019-11-16 21:44:28 -0600

    Imported  json.simple

--- MemberWriter.java
+++ MemberWriter.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 import java.beans.XMLEncoder;
 import java.io.*;
+import org.json.simple.*;
+
 public class MemberWriter {
 
 
 
-	//prints members to screen
+    //prints members to screen
 
     public static void printMembersToScreen(ArrayList<Member> members){
         for(Member m: members){
@@ -19,7 +21,7 @@ public class MemberWriter {
         try{
             PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fname)));
             for(Member m: members){
-            	//pw.println(m);
+                //pw.println(m);
                 pw.println(m.toStringTabs());
             }
             pw.close();
@@ -33,29 +35,29 @@ public class MemberWriter {
 
     //function to write to binary
     public static boolean writeMembersToBinary(String fname, ArrayList<Member> members){
-    	try {
-    		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fname));
-    		oos.writeObject(members);
-    		oos.close();
-    		System.out.println("\nMembers Written Successfully");
-    		return true;
-    	} catch (Exception ex) {
-    		return false;
-    	}
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fname));
+            oos.writeObject(members);
+            oos.close();
+            System.out.println("\nMembers Written Successfully");
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     //function to write to xml
     public static boolean writeMembersToXML(String fname, ArrayList<Member> members) {
-    	try {
-    		XMLEncoder enc = new XMLEncoder(new BufferedOutputStream(new 
-    				FileOutputStream(fname)));
-    		enc.writeObject(members);
-    		enc.close();
-    		System.out.println("\nMembers Written Successfully");
-    		return true;
-    	} catch (Exception ex) {
-    		return false;
-    	}
+        try {
+            XMLEncoder enc = new XMLEncoder(new BufferedOutputStream(new
+                    FileOutputStream(fname)));
+            enc.writeObject(members);
+            enc.close();
+            System.out.println("\nMembers Written Successfully");
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
     //function to write to JSON
     public static boolean writeMembersToJSON(String fname, ArrayList<InsuranceScore> members) {
@@ -81,5 +83,6 @@ public class MemberWriter {
             return false;
         }
     }
+
 
 }
